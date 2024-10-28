5a738d0b5e22af6e4a41f2fc599dcd3b780aef85
gemson.arzola <gemson.arzola@gmail.com>
2024-10-28 14:36:39 +0000

    update

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,16 @@
 <template>
-<div>
-    <v-container>
-        <v-btn rounded :loading="loading">Hello</v-btn>
-    </v-container>
-</div>    
+    <div>
+        <v-container>
+            <v-btn rounded :loading="loading" color="primary" icon>
+                <v-icon>mdi-home</v-icon>
+            </v-btn>
+        </v-container>
+    </div>
 </template>
 <script setup lang="ts">
 const loading = ref(false)
 
-onMounted( () => {
-    loading.value =true
+onMounted(() => {
+    // loading.value =true
 })
 </script>
