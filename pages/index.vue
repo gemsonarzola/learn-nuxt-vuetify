2e21c9866652c8d1cb74196f73f0d9f7e6a2e4e1
gemson.arzola <gemson.arzola@gmail.com>
2024-10-28 14:24:13 +0000

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+<div>
+    <v-container>
+        <v-btn rounded :loading="loading">Hello</v-btn>
+    </v-container>
+</div>    
+</template>
+<script setup lang="ts">
+const loading = ref(false)
+
+onMounted( () => {
+    loading.value =true
+})
+</script>
